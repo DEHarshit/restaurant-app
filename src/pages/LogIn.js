348e2f71ed3cf111e319c5279d8d2a01684c43fc
DEHarshit <harshit.s2004@gmail.com>
2024-06-10 22:35:44 +0530

    added transition

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,8 +1,14 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 export default function LogIn() {
     const [log, setLog] = useState(true);
     const [visible, setVisible] = useState(false);
+    const [transition,setTransition] = useState(false);
+    function handleLog(){
+        setTransition(true)
+        setTimeout(() => setLog(!log), 1000);
+        setTimeout(() => setTransition(false), 1500);
+    }
     return (
         <div className="relative h-screen flex justify-center items-center">
             <div className='fixed inset-0 animate-pulse'>
@@ -15,7 +21,7 @@ export default function LogIn() {
                     </div>
                 </div>
             </div>
-            <div className="relative inset-0 h-screen w-screen flex items-center justify-center font-primary">
+            <div className="relative w-screen flex justify-center font-primary">
                 <div className="bg-gradient-to-r from-[#CEA07E] to-[#BB5656] rounded-lg flex items-center justify-center p-1">
                     {(log) ? (
                         <div className='flex transition-all'>
@@ -24,13 +30,13 @@ export default function LogIn() {
                                 backgroundSize: "cover",
                                 backgroundPosition: "center"
                             }} className='h-[485px] w-[485px] rounded-l-lg bg-blue-100 border-r border-zinc-600'>
-                                <div className="w-full h-full bg-black hover:opacity-0 transition-all opacity-60 flex items-center justify-center text-7xl font-semibold tracking-wider">
+                                <div className="rounded-l-lg w-full h-full bg-black hover:opacity-0 transition-all opacity-60 flex items-center justify-center text-7xl font-semibold tracking-wider">
                                     LOG IN
                                 </div>
                             </div>
                             <div className='flex flex-col px-12 py-12 space-y-[50px] bg-zinc-900 h-[485px] w-[485px] rounded-r-lg'>
                                 <h2 className="text-3xl font-bold tracking-wider leading-9 space-x-2">
-                                    <span className="bg-gradient-to-r from-[#CEA07E] to-[#BB5656] text-transparent inline-block bg-clip-text">
+                                    <span className="bg-gradient-to-r from-[#CEA07E] to-[#BB5656] text-transparent inline-block bg-clip-text ">
                                         LOG
                                     </span>
                                     <span>IN</span>
@@ -66,7 +72,7 @@ export default function LogIn() {
                                 </div>
                                 <div className="flex flex-col space-y-2 justify-center items-center">
                                     <button type="button" className=" w-fit h-fit bg-zinc-400 hover:text-white text-lg text-black p-2 px-10 font-semibold transition-all duration-400 hover:text-xl rounded-full hover:bg-gradient-to-r from-[#CEA07E] to-[#BB5656]">Log In</button>
-                                    <button type="button" className="text-zinc-400" onClick={() => (setLog(!log))}>Sign in?</button>
+                                    <button type="button" className="text-zinc-400" onClick={handleLog}>Sign in?</button>
                                 </div>
                             </div>
                         </div>
@@ -135,7 +141,7 @@ export default function LogIn() {
                                 </span>
                                 <div className="flex flex-col translate-y-4 space-y-2 justify-center items-center">
                                     <button type="button" className=" w-fit h-fit bg-zinc-400 text-lg text-black hover:text-white p-2 px-10 font-semibold transition-all duration-400 hover:text-xl rounded-full hover:bg-gradient-to-r from-[#CEA07E] to-[#BB5656]">Sign In</button>
-                                    <button type="button" className="text-zinc-400" onClick={() => (setLog(!log))}>Log in?</button>
+                                    <button type="button" className="text-zinc-400" onClick={handleLog}>Log in?</button>
                                 </div>
                             </div>
                             <div style={{
@@ -143,12 +149,14 @@ export default function LogIn() {
                                 backgroundSize: "cover",
                                 backgroundPosition: "center"
                             }} className='h-[485px] w-[485px] rounded-r-lg bg-blue-100  border-l border-zinc-600'>
-                                <div className="w-full h-full bg-black hover:opacity-0 transition-all opacity-60 flex items-center justify-center text-7xl font-semibold tracking-wider">
+                                <div className="rounded-r-lg w-full h-full bg-black hover:opacity-0 transition-all opacity-60 flex items-center justify-center text-7xl font-semibold tracking-wider">
                                     SIGN IN
                                 </div>
                             </div>
                         </div>
                     )}
+                </div>
+                <div className={`${transition ? 'h-[490px]' : 'opacity-0 h-[1px]'} transition-all duration-1000 absolute flex bg-gradient-to-r from-[#CEA07E] to-[#BB5656] w-[970px] rounded-lg `}>
                 </div>
             </div>
         </div>
