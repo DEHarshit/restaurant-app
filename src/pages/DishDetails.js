6ed92685348dde4a3eaffb6fb50aebad8a3881fd
sivadarshini3112 <sivadarshini@ptuniv.edu.in>
2024-06-15 17:46:35 +0530

    added dish details page

--- src/pages/DishDetails.js
+++ src/pages/DishDetails.js
@@ -1,29 +1,52 @@
 export default function DishDetails() {
-    return (
-      <div className="font-primary flex justify-center items-center h-screen">
-        <div
+  return (
+      <div
           style={{
-            backgroundImage: "url(/home-image.jpg)",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
+              backgroundImage: "url(/menu-bg-1.png)",
           }}
-          className="bg-blue-100 h-[485px] w-[485px] rounded-l-lg"
-        ></div>
-        <div className="flex flex-col bg-zinc-800 h-[485px] w-[485px] p-10 space-y-10 rounded-r-lg">
-          <h2 className="text-5xl text-black font-semibold tracking-wider">
-            Pasta
-          </h2>
-          <h2 className="text-2xl text-green-500">$00.00</h2>
-          <h2 className="text-md text-zinc-600">Vegetables, eggs, fish, meat</h2>
-          <button
-            className="bg-gradient-to-r from-[#CEA07E] to-[#BB5656] p-3 rounded-full hover:bg-gradient-to-l transition-all"
-            type="button"
-          >
-            Add to cart
-          </button>
-        </div>
-        
+          className="font-primary flex justify-center items-center h-screen"
+      >
+          <div
+              style={{
+                  backgroundImage: "url(/dishes/pasta.jpg)",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+              }}
+              className="bg-blue-100 h-[550px] w-[550px] rounded-l-lg"
+          ></div>
+          <div className="flex flex-col bg-zinc-800 h-[550px] w-[550px] p-10 space-y-10 rounded-r-lg">
+              <h2 className="text-5xl text-white-100 font-semibold tracking-medium">
+                  Pasta
+                  <hr className="w-[150px] h-0.5 border-0 bg-gradient-to-r from-[#CEA07E] to-[#BB5656]"></hr>
+              </h2>
+              <p style={{
+                    backgroundImage: 'linear-gradient(to right, white, #BB5656)',
+                    WebkitBackgroundClip: 'text',
+                    backgroundClip: 'text',
+                    color: 'transparent',
+                    
+              }} className="font-primary font-semibold text-3xl"  >
+                          Elevate Your Palate!!
+                      </p>
+
+                <p className="text-md text-gray-400">
+                   Pasta,Olive oil, Garlic,Onion,Tomatoes,Basil,
+                   Parmesan Cheese,Salt and Pepper
+                </p>
+                <div className="flex items-center mb-6 ">
+                    <h2 className="font-primary text-3xl text-white-100 mr-4 ">₹170</h2>
+                    <input
+                      type="number"
+                      className="font-primary text-2xl bg-zinc-800 border border-gray-300 rounded-r-lg rounded-l-lg px-3 py-1 w-16 text-center text-white"
+                    />
+                </div>
+
+              <button className="font-primary font-semibold text-2xl w-[150px] rounded-l-lg rounded-r-lg bg-gradient-to-r hover:bg-gradient-to-b from-[#CEA07E] to-[#BB5656] p-2 ">
+                                            Add to Cart
+              </button>
+
+
+          </div>
       </div>
-    );
-  }
-  
+  );
+}
